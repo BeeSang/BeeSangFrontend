56b27b5aafe57c91d4928fa445c727dfa16503a9
zzzzseong <jung48182@gmail.com>
2023-09-27 22:17:31 +0900

    feat: welcome page 구성 완료

--- app/src/main/java/com/example/beesang/MainActivity.kt
+++ app/src/main/java/com/example/beesang/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.beesang.ui.theme.BeeSangTheme
 
+import com.example.beesang.welcome.Welcome
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +25,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+//                    Greeting("Android")
+                    Welcome()
                 }
             }
         }
