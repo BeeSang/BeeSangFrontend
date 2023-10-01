11e2c7953895947090330662c9362415c9c1ccb4
zzzzseong <jung48182@gmail.com>
2023-10-01 19:37:56 +0900

    feat: welcome -> login activity 이동 구현 완료

--- app/src/main/java/com/example/beesang/MainActivity.kt
+++ app/src/main/java/com/example/beesang/MainActivity.kt
@@ -8,10 +8,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.beesang.activitys.LoginActivity
 import com.example.beesang.login.Login
 import com.example.beesang.ui.theme.BeeSangTheme
 
@@ -28,34 +28,25 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-//                    val intent = Intent(this, Welcome(
-//                        onButtonTapped = {
-//                            Log.d("MainActivity", "onButtonTapped")
-//                        }
-//                    )::class.java)
-//                    startActivity(intent)
-
-                    Welcome(onButtonTapped = {
-                        Log.d("MainActivity", "onButtonTapped")
-                    })
+                    Welcome(onButtonTapped = { startBtnClick() })
                 }
             }
         }
     }
+
+    private fun startBtnClick() {
+        Intent(this, LoginActivity::class.java).also {
+            startActivity(it)
+        }
+    }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun WelcomePreview() {
     BeeSangTheme {
-        Greeting("Android")
+        Welcome()
     }
 }
--- app/src/main/java/com/example/beesang/activitys/LoginActivity.kt
+++ app/src/main/java/com/example/beesang/activitys/LoginActivity.kt
@@ -1,7 +1,41 @@
 package com.example.beesang.activitys
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.beesang.login.Login
+import com.example.beesang.ui.theme.BeeSangTheme
 
 class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BeeSangTheme {
+                // A surface container using the 'background' color from the theme
+                // initial commit2
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Login()
+                }
+            }
+        }
+    }
+}
 
+
+
+@Preview(showBackground = true)
+@Composable
+fun LoginPreview() {
+    BeeSangTheme {
+        Login()
+    }
 }
